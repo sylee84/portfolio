8db56c9b5d51b1e5322ab8bfcbcd72f0fa469d54
sylee84 <46161902+sylee84@users.noreply.github.com>
2019-03-18 16:56:33 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
 var $body;
 $(document).ready(function() {
   $body = $('body');
+  bodyHide();
   windowRwd();
   cardTrans();
   cardSwiper();
@@ -10,7 +11,6 @@ $(document).ready(function() {
 });
 function windowRwd(){
   window.onload = function() {
-    $body.hide().fadeIn(1500);
     if ($(window).width() < 800){
       $body.addClass('mobile').removeClass('pc');
       //document.location.href = "";
@@ -27,9 +27,14 @@ function windowRwd(){
     }
   }
 }
+function bodyHide(){
+  jQuery.event.add(window,"load",function(){
+    $body.hide().fadeIn(1500);
+  });
+}
 function cardTrans(){
   $('.click').click(function(){
-    var $this = $(this);
+   var $this = $(this);
     if(!$this.parent().hasClass('on')){
       $this.parent().addClass('on');
     }else{
